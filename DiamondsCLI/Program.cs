925ddd67b6b1caa73f609ac3407e5dd2ab9e040c
Slawomir Idzikowski <s.idzikowski@hotmail.com>
2019-09-15 18:59:32 +0200

    Add project files.

---
+++ DiamondsCLI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DiamondsCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Należy podać login oraz hasło");
+            }
+            else
+            {
+                Console.WriteLine("Login: "+args[0]+" Haslo: "+args[1]);
+            }
+            
+        }
+    }
+}
